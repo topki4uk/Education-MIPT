4586c26db9574c5107c2ce0c5e01a17811166da3
Matthias <i.s.m.mipt@yandex.ru>
2026-04-25 19:31:34 +0300

    Доработан пример 14.49.

--- projects/examples/source/14.49.cpp
+++ projects/examples/source/14.49.cpp
@@ -1,35 +1,34 @@
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
 // chapter : Parallelism
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
 // section : Atomics
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
 // content : Lock-Free Programming
 //
 // content : Thread-Safe Lock-Free Stacks
 //
-// content : Garbage Collectors
+// content : Hazard Pointers
 //
-// content : Thread Reference Counting
+// content : Garbage Collectors
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
+#include <array>
 #include <atomic>
 #include <cstddef>
 #include <functional>
-#include <memory>
 #include <thread>
-#include <tuple>
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
 #include <boost/noncopyable.hpp>
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
 template < typename T > class Stack : private boost::noncopyable
 {
@@ -42,6 +41,168 @@ private :
         Node * next = nullptr;
     };
 
+//  ---------------------------------------------------------------------------------
+
+    struct Pointer 
+    {
+        std::atomic < std::thread::id > id = std::thread::id();
+
+        std::atomic < Node * > node = nullptr;
+    };
+
+//  ---------------------------------------------------------------------------------
+
+    class Handler
+    {
+    public :
+
+        Handler() : m_pointer(nullptr)
+        {
+            for (auto & pointer : s_pointers)
+            {
+                std::thread::id id;
+
+                if
+                (
+                    pointer.id.compare_exchange_strong
+                    (
+                        id, std::this_thread::get_id(),
+
+                        std::memory_order::acquire,
+
+                        std::memory_order::relaxed
+                    )
+                )
+                {
+                    m_pointer = &pointer;
+
+                    break;
+                }
+            }
+        }
+
+    //  -----------------------------------------------------------------------
+
+       ~Handler()
+        {
+            m_pointer->node.store(nullptr, std::memory_order::relaxed);
+
+            m_pointer->id.store(std::thread::id(), std::memory_order::release);
+        }
+
+    //  -----------------------------------------------------------------------
+
+        auto & get() const 
+        {
+            return m_pointer->node;
+        }
+
+    private :
+
+        Pointer * m_pointer = nullptr;
+    };
+
+//  ---------------------------------------------------------------------------------
+
+    class Storage
+    {
+    private :
+
+        class Retired_Node
+        {
+        public :
+
+           ~Retired_Node()
+            {
+                delete node;
+            }
+
+        //  ------------------------------
+
+            Node * node = nullptr;
+
+            Retired_Node * next = nullptr;
+        };
+
+    public :
+
+        void push_back(Node * node)
+        {
+            push_back(new Retired_Node(node, nullptr));
+
+            m_size.fetch_add(1, std::memory_order::relaxed);
+        }
+
+    //  -----------------------------------------------------------------------------
+
+        void try_clear() 
+        {
+            if (m_size.load(std::memory_order::relaxed) >= std::size(s_pointers) * 2)
+            {
+                clear();
+            }
+        }
+
+    private :
+
+        void push_back(Retired_Node * node) 
+        {
+            node->next = m_head.load(std::memory_order::relaxed);
+
+            while 
+            (
+                !m_head.compare_exchange_weak
+                (
+                    node->next, node,
+
+                    std::memory_order::release,
+
+                    std::memory_order::relaxed
+                )
+            );
+        }
+
+    //  -----------------------------------------------------------------------------
+
+        void clear() 
+        {
+            auto node = m_head.exchange(nullptr, std::memory_order::acquire);
+
+            m_size.store(0, std::memory_order_relaxed);
+
+            auto size = 0uz;
+
+            while (node)
+            {
+                auto next = node->next;
+
+                if (!has_pointers(node->node)) 
+                {
+                    delete node;
+                }
+                else 
+                {
+                    push_back(node);
+
+                    ++size;
+                }
+
+                node = next;
+            }
+
+            if (size > 0) 
+            {
+                m_size.fetch_add(size, std::memory_order_relaxed);
+            }
+        }
+
+    //  -----------------------------------------------------------------------------
+
+        std::atomic < Retired_Node * > m_head = nullptr;
+
+        std::atomic < std::size_t > m_size = 0;
+    };
+
 public :
 
    ~Stack()
@@ -51,7 +212,7 @@ public :
         while (top_and_pop(x));
     }
 
-//  -------------------------------------------------------------------------
+//  ---------------------------------------------------------------------------------
 
     void push(T x)
     {
@@ -72,17 +233,31 @@ public :
         );
     }
 
-//  -------------------------------------------------------------------------
+//  ---------------------------------------------------------------------------------
 
     auto top_and_pop(T & x)
     {
-        m_counter.fetch_add(1, std::memory_order::acquire);
+        auto & pointer = get_pointer();
 
         auto head = m_head.load(std::memory_order::acquire);
 
-        while 
+        do 
+        {
+            Node * node = nullptr;
+
+            do 
+            {
+                node = head;
+
+                pointer.store(head);
+
+                head = m_head.load(std::memory_order::acquire);
+            }
+            while (head != node);
+        }
+        while
         (
-            head && !m_head.compare_exchange_weak
+            head && !m_head.compare_exchange_strong
             (
                 head, head->next,
 
@@ -92,129 +267,100 @@ public :
             )
         );
 
+        pointer.store(nullptr, std::memory_order::release);
+
         if (head)
         {
             x = head->x;
 
-            try_free(head);
+            if (has_pointers(head))
+            {
+                m_storage.push_back(head);
+            }
+            else
+            {
+                delete head;
+            }
+
+            m_storage.try_clear();
 
             return true;
         }
-
-        m_counter.fetch_sub(1, std::memory_order::release);
 
         return false;
     }
 
 private :
 
-    void try_free(Node * head)
+    auto & get_pointer() 
     {
-        if (m_counter.load(std::memory_order::acquire) == 1)
-        {
-            auto tail = m_tail.exchange(nullptr, std::memory_order::acquire);
+        thread_local static Handler handler;
 
-            if (!(m_counter.fetch_sub(1, std::memory_order::acq_rel) - 1))
+        return handler.get();
+    }
+
+//  ---------------------------------------------------------------------------------
+
+    static auto has_pointers(Node * node)
+    {
+        for (auto & pointer : s_pointers)
+        {
+            if (pointer.id.load(std::memory_order::acquire) != std::thread::id())
             {
-                free(tail);
+                if (pointer.node.load(std::memory_order::relaxed) == node) 
+                {
+                    return true;
+                }
             }
-            else if (tail)
-            {
-                save(tail);
-            }
-
-            delete head;
-        }
-        else
-        {
-            save(head, head);
-
-            m_counter.fetch_sub(1, std::memory_order::release);
-        }
-    }
-
-//  -------------------------------------------------------------------------
-
-    void free(Node * nodes) const
-    {
-        while (nodes)
-        {
-            auto next = nodes->next;
-
-            delete nodes;
-
-            nodes = next;
-        }
-    }
-
-//  -------------------------------------------------------------------------
-
-    void save(Node * nodes)
-    {
-        auto end = nodes;
-
-        while(auto next = end->next)
-        {
-            end = next;
         }
 
-        save(nodes, end);
+        return false;
     }
 
-//  -------------------------------------------------------------------------
+//  ---------------------------------------------------------------------------------
 
-    void save(Node * begin, Node * end)
-    {
-        end->next = m_tail;
+    std::atomic < Node * > m_head = nullptr;
 
-        while
-        (
-            !m_tail.compare_exchange_weak
-            (
-                end->next, begin,
+    Storage m_storage;
 
-                std::memory_order::release, 
+//  ---------------------------------------------------------------------------------
 
-                std::memory_order::relaxed                
-            )
-        );
-    }
-
-//  -------------------------------------------------------------------------
-
-    std::atomic < Node * > m_head = nullptr, m_tail = nullptr;
-
-    std::atomic < std::size_t > m_counter = 0;
+    static inline std::array < Pointer, 64 > s_pointers = {};
 };
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
-void top_and_pop(Stack < int > & stack)
+void produce(Stack < int > & stack)
+{
+    for (auto i = 1; i < (1 << 10) + 1; ++i)
+    {
+        stack.push(i);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+void consume(Stack < int > & stack)
 {
     auto x = 0;
 
-    stack.top_and_pop(x);
+    for (auto i = 0uz; i < 1 << 10; ++i)
+    {
+        stack.top_and_pop(x);
+    }
 }
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
     Stack < int > stack;
 
-//  ---------------------------------------------------------
+//  ------------------------------------------------
 
-    stack.push(1);
+    std::jthread thread_1(produce, std::ref(stack));
 
-    stack.push(2);
-
-//  ---------------------------------------------------------
-
-    {
-        std::jthread jthread_1(top_and_pop, std::ref(stack));
-
-        std::jthread jthread_2(top_and_pop, std::ref(stack));
-    }
+    std::jthread thread_2(consume, std::ref(stack));
 }
 
-/////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
