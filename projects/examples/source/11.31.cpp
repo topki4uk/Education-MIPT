e57beea11363f5381227a57e94c10c2a1aa37ac7
Matthias <i.s.m.mipt@yandex.ru>
2026-05-01 21:20:20 +0300

    Доработаны примеры модуля 11.

--- projects/examples/source/11.31.cpp
+++ projects/examples/source/11.31.cpp
@@ -8,13 +8,13 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// content : Depth First Search (DFS) Algorithm
+// content : Breadth First Search (BFS) Algorithm
 //
 // content : Time Complexity O(V + E)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// support : www.cs.usfca.edu/~galles/visualization/DFS.html
+// support : www.cs.usfca.edu/~galles/visualization/BFS.html
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -23,92 +23,92 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/breadth_first_search.hpp>
 #include <boost/graph/named_function_params.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class Visitor : public boost::default_dfs_visitor
+class Visitor : public boost::default_bfs_visitor
 {
 public :
 
-    void initialize_vertex(auto vertex, auto const & graph)
+    auto initialize_vertex(auto vertex, auto const & graph)
     {
         std::cout << "Visitor::initialize_vertex : vertex = " << vertex << '\n';
 
-        boost::default_dfs_visitor::initialize_vertex(vertex, graph);
+        return boost::default_bfs_visitor::initialize_vertex(vertex, graph);
     }
 
 //  ----------------------------------------------------------------------------
 
-    void start_vertex(auto vertex, auto const & graph)
-    {
-        std::cout << "Visitor::start_vertex : vertex = " << vertex << '\n';
-
-        boost::default_dfs_visitor::start_vertex(vertex, graph);
-    }
-
-//  ----------------------------------------------------------------------------
-
-    void discover_vertex(auto vertex, auto const & graph)
+    auto discover_vertex(auto vertex, auto const & graph)
     {
         std::cout << "Visitor::discover_vertex : vertex = " << vertex << '\n';
 
-        boost::default_dfs_visitor::discover_vertex(vertex, graph);
+        return boost::default_bfs_visitor::discover_vertex(vertex, graph);
     }
 
 //  ----------------------------------------------------------------------------
 
-    void finish_vertex(auto vertex, auto const & graph)
+    auto examine_vertex(auto vertex, auto const & graph)
+    {
+        std::cout << "Visitor::examine_vertex : vertex = " << vertex << '\n';
+
+        return boost::default_bfs_visitor::examine_vertex(vertex, graph);
+    }
+
+//  ----------------------------------------------------------------------------
+
+    auto finish_vertex(auto vertex, auto const & graph)
     {
         std::cout << "Visitor::finish_vertex : vertex = " << vertex << '\n';
 
-        boost::default_dfs_visitor::finish_vertex(vertex, graph);
+        return boost::default_bfs_visitor::finish_vertex(vertex, graph);
     }
 
 //  ----------------------------------------------------------------------------
 
-    void examine_edge(auto edge, auto const & graph)
+    auto examine_edge(auto edge, auto const & graph)
     {
         std::cout << "Visitor::examine_edge : edge = " << edge << '\n';
 
-        boost::default_dfs_visitor::examine_edge(edge, graph);
+        return boost::default_bfs_visitor::examine_edge(edge, graph);
     }
 
 //  ----------------------------------------------------------------------------
 
-    void tree_edge(auto edge, auto const & graph)
+    auto tree_edge(auto edge, auto const & graph)
     {
         std::cout << "Visitor::tree_edge : edge = " << edge << '\n';
 
-        boost::default_dfs_visitor::tree_edge(edge, graph);
+        return boost::default_bfs_visitor::tree_edge(edge, graph);
     }
 
 //  ----------------------------------------------------------------------------
 
-    void back_edge(auto edge, auto const & graph)
+    auto non_tree_edge(auto edge, auto const & graph)
     {
-        std::cout << "Visitor::back_edge : edge = " << edge << '\n';
+        std::cout << "Visitor::non_tree_edge : edge = " << edge << '\n';
 
-        boost::default_dfs_visitor::back_edge(edge, graph);
+        return boost::default_bfs_visitor::non_tree_edge(edge, graph);
     }
 
 //  ----------------------------------------------------------------------------
 
-    void forward_or_cross_edge(auto edge, auto const & graph)
+    auto gray_target(auto edge, auto const & graph)
     {
-        std::cout << "Visitor::forward_or_cross_edge : edge = " << edge << '\n';
+        std::cout << "Visitor::gray_target : edge = " << edge << '\n';
 
-        boost::default_dfs_visitor::forward_or_cross_edge(edge, graph);
+        return boost::default_bfs_visitor::gray_target(edge, graph);
     }
 
 //  ----------------------------------------------------------------------------
 
-    void finish_edge(auto edge, auto const & graph)
+    auto black_target(auto edge, auto const & graph)
     {
-        std::cout << "Visitor::finish_edge : edge = " << edge << '\n';
+        std::cout << "Visitor::black_target : edge = " << edge << '\n';
 
-        boost::default_dfs_visitor::finish_edge(edge, graph);
+        return boost::default_bfs_visitor::black_target(edge, graph);
     }
 };
 
@@ -116,31 +116,25 @@ public :
 
 int main()
 {
-    boost::adjacency_list < boost::vecS, boost::vecS, boost::directedS > graph;
+	boost::adjacency_list < boost::vecS, boost::vecS, boost::directedS > graph;
 
 //  ---------------------------------------------------------------------------
 
-    boost::add_edge(0, 2, graph);
+    boost::add_edge(0, 1, graph);
 
-    boost::add_edge(1, 1, graph);
+    boost::add_edge(1, 2, graph);
 
     boost::add_edge(1, 3, graph);
 
-    boost::add_edge(2, 1, graph);
-
-    boost::add_edge(2, 3, graph);
-
-    boost::add_edge(3, 1, graph);
+    boost::add_edge(2, 4, graph);
 
     boost::add_edge(3, 4, graph);
 
-    boost::add_edge(4, 0, graph);
-
-    boost::add_edge(4, 1, graph);
+    boost::add_edge(4, 5, graph);
 
 //  ---------------------------------------------------------------------------
 
-    boost::depth_first_search(graph, boost::visitor(Visitor()).root_vertex(1));
+    boost::breadth_first_search(graph, 0, boost::visitor(Visitor()));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
