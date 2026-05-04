be2b207443d07e1c51dc3c47b84c3a92063d6bf1
Matthias <i.s.m.mipt@yandex.ru>
2026-05-04 12:10:21 +0300

    Доработаны отдельные примеры.

--- projects/examples/source/12.06.cpp
+++ projects/examples/source/12.06.cpp
@@ -1,75 +1,75 @@
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 // chapter : Characters
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 // section : Strings
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 // content : Strings
 //
-// content : String std::string
+// content : Strings std::string and std::wstring
 //
-// content : Function std::getline
+// content : Strings std::u8string, std::u16string and std::u32string
 //
-// content : Manipulator std::ws
+// content : Literal Prefixes L, u8, u and U
 //
-// content : Index-Based Algorithms
+// content : Operator ""s
+//
+// content : Namespace std::literals
 //
 // content : God Class Problem
-//
-// content : Functions std::stoi and std::to_string
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 // support : Boost.StringAlgorithms
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
 
 #include <cassert>
-#include <istream>
-#include <sstream>
+#include <iostream>
+#include <print>
 #include <string>
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+using namespace std::literals;
+
+/////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    std::stringstream stream("aaaaa aaaaa");
+    auto string_1 =   "aaaaa"s;
 
-//  ----------------------------------------------
+    auto string_2 =  L"aaaaa"s;
 
-    std::string string;
+    auto string_3 = u8"aaaaa"s;
 
-//  ----------------------------------------------
+    auto string_4 =  u"aaaaa"s;
 
-    stream >> string;
+    auto string_5 =  U"aaaaa"s;
 
-//  ----------------------------------------------
+//  --------------------------------------------------------
 
-    assert(string == "aaaaa");
+    std::print("main : string_1 = {}\n", string_1);
 
-//  ----------------------------------------------
+//	std::print("main : string_2 = {}\n", string_2); // error
 
-    stream.seekg(0);
+//	std::print("main : string_3 = {}\n", string_3); // error
 
-//  ----------------------------------------------
+//	std::print("main : string_4 = {}\n", string_4); // error
 
-    std::getline(stream >> std::ws, string);
+//	std::print("main : string_5 = {}\n", string_5); // error
 
-//  ----------------------------------------------
+//  --------------------------------------------------------
 
-    assert(string == "aaaaa aaaaa");
+	std::wcout << "main : string_2 = " << string_2 << '\n';
 
-//  ----------------------------------------------
+//  --------------------------------------------------------
 
-    assert(string.find('a') != std::string::npos);
-
-//  ----------------------------------------------
-
-    assert(std::stoi(std::to_string(1)) == 1);
+    assert(string_1.find('a') != std::string::npos);
 }
 
-///////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////
