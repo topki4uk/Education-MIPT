17b7d03ab55e641eab0c2d21f35d61257b57f80d
Matthias <i.s.m.mipt@yandex.ru>
2026-05-04 12:05:44 +0300

    Доработаны отдельные примеры.

--- projects/examples/source/12.07.cpp
+++ projects/examples/source/12.07.cpp
@@ -1,51 +1,85 @@
-/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
 // chapter : Characters
 
-/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
 // section : Strings
 
-/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
-// content : Small Strings Optimization (SSO)
+// content : String std::basic_string
+//
+// content : User-Defined Case-Insensitive Strings
+//
+// content : Helper std::char_traits
+//
+// content : Function std::tolower
 
-/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
-#include <bit>
 #include <cassert>
+#include <cctype>
 #include <cstddef>
+#include <memory>
 #include <string>
 
-/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
 
-auto distance(auto * x, auto * y)
+class Traits : public std::char_traits < char >
 {
-    return
-    (
-        std::bit_cast < std::byte * > (x) -
+public :
 
-        std::bit_cast < std::byte * > (y)
-    );
-}
+    static auto eq(char x, char y) { return std::tolower(x) == std::tolower(y); }
 
-/////////////////////////////////////////////////////////
+    static auto lt(char x, char y) { return std::tolower(x) <  std::tolower(y); }
+
+//  -----------------------------------------------------------------------------------
+
+    static auto compare(char const * string_1, char const * string_2, std::size_t size)
+    {
+        for (auto i = 0uz; i < size; ++i)
+        {
+            if (!eq(string_1[i], string_2[i]))
+            {
+                return lt(string_1[i], string_2[i]) ? -1 : +1;
+            }
+        }
+
+        return 0;
+    }
+
+//  -----------------------------------------------------------------------------------
+
+    static auto find(char const * string, std::size_t size, char x) -> char const *
+    {
+        for (auto i = 0uz; i < size; ++i)
+        {
+            if (eq(string[i], x))
+            {
+                return &string[i];
+            }
+        }
+
+        return nullptr;
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
-    std::string string_1(15, 'a');
+    using string_t = std::basic_string < char, Traits, std::allocator < char > > ;
 
-    std::string string_2(16, 'a');
+//  ------------------------------------------------------------------------------
 
-//  -----------------------------------------------------
+    assert(string_t("aaaaa") == string_t("AAAAA"));
 
-    static_assert(sizeof(std::string) == 32);
+    assert(string_t("bbbbb") == string_t("BBBBB"));
 
-//  -----------------------------------------------------
+    assert(string_t("aaaaa") != string_t("bbbbb"));
 
-    assert(distance(&string_1.front(), &string_1) == 16);
-
-    assert(distance(&string_2.front(), &string_2) != 16);
+    assert(string_t("AAAAA") != string_t("BBBBB"));
 }
 
-/////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
--- projects/examples/source/12.08.cpp
+++ projects/examples/source/12.08.cpp
@@ -1,85 +1,51 @@
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
 // chapter : Characters
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
 // section : Strings
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
-// content : String std::basic_string
-//
-// content : User-Defined Case-Insensitive Strings
-//
-// content : Helper std::char_traits
-//
-// content : Function std::tolower
+// content : Small Strings Optimization (SSO)
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
+#include <bit>
 #include <cassert>
-#include <cctype>
 #include <cstddef>
-#include <memory>
 #include <string>
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
-class Traits : public std::char_traits < char >
+auto distance(auto * x, auto * y)
 {
-public :
+    return
+    (
+        std::bit_cast < std::byte * > (x) -
 
-    static auto eq(char x, char y) { return std::tolower(x) == std::tolower(y); }
+        std::bit_cast < std::byte * > (y)
+    );
+}
 
-    static auto lt(char x, char y) { return std::tolower(x) <  std::tolower(y); }
-
-//  -----------------------------------------------------------------------------------
-
-    static auto compare(char const * string_1, char const * string_2, std::size_t size)
-    {
-        for (auto i = 0uz; i < size; ++i)
-        {
-            if (!eq(string_1[i], string_2[i]))
-            {
-                return lt(string_1[i], string_2[i]) ? -1 : +1;
-            }
-        }
-
-        return 0;
-    }
-
-//  -----------------------------------------------------------------------------------
-
-    static auto find(char const * string, std::size_t size, char x) -> char const *
-    {
-        for (auto i = 0uz; i < size; ++i)
-        {
-            if (eq(string[i], x))
-            {
-                return &string[i];
-            }
-        }
-
-        return nullptr;
-    }
-};
-
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
 
 int main()
 {
-    using string_t = std::basic_string < char, Traits, std::allocator < char > > ;
+    std::string string_1(15, 'a');
 
-//  ------------------------------------------------------------------------------
+    std::string string_2(16, 'a');
 
-    assert(string_t("aaaaa") == string_t("AAAAA"));
+//  -----------------------------------------------------
 
-    assert(string_t("bbbbb") == string_t("BBBBB"));
+    static_assert(sizeof(std::string) == 32);
 
-    assert(string_t("aaaaa") != string_t("bbbbb"));
+//  -----------------------------------------------------
 
-    assert(string_t("AAAAA") != string_t("BBBBB"));
+    assert(distance(&string_1.front(), &string_1) == 16);
+
+    assert(distance(&string_2.front(), &string_2) != 16);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////
